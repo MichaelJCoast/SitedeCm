8e4a3d93ee1294eae975a6c853a08e33020880ab
elcascavel <simao.ramaral@gmail.com>
2022-05-21 19:18:53 +0100

    NECM Members database and page

---
+++ database/migrations/2022_05_21_171906_create_members_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('image')->unique();
+            $table->string('name')->unique();
+            $table->string('department')->unique();
+            $table->string('role')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+
+Route::view("equipa",'member');
