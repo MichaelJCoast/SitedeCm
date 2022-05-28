b93d0faf8578f2d31e8d09e9b17159784d1b8e47
elcascavel <simao.ramaral@gmail.com>
2022-05-28 21:33:39 +0100

    Commits are now responsive

--- resources/views/links.blade.php
+++ resources/views/links.blade.php
@@ -1,16 +1,19 @@
 @extends('layouts.main')
 @section('content')
-<div class="min-h-screen">
+<div class="items-center mt-6">
+  <h1 class="font-bold text-center text-5xl">Links</h1>
+</div>
 <div class="py-6 flex items-center justify-center">
-<div class="grid grid-cols-1 gap-12">
-<h1 class="font-bold text-center text-5xl">Links</h1>
-@foreach($link as $key => $data)
-<a type="button" href="{{$data->link_to}}" class="py-6 px-52 bg-black border border-white hover:bg-red-800 text-white h-20 transition ease-in duration-200 text-center text-xl font-semibold shadow-md rounded-lg">
-  {{$data->name}}
-</a>
-@endforeach
-</div>
-</div>
-</div>
-@include('layouts.footer')
-@endsection
+    <div class="grid grid-cols-1 gap-8 min-w-max">
+            @foreach($link as $key => $data)
+            <div class="p-6 border-l-4 border-red-500 bg-black">
+                <div class="flex items-center justify-between cursor-pointer">
+                    <a href="{{$data->link_to}}" class="text-lg text-center font-medium text-white">
+                        {{$data->name}}
+                    </a>
+            </div>
+        </div>
+        @endforeach
+    </div>
+    @include('layouts.footer')
+    @endsection
