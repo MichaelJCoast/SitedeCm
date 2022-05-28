081d41b5b33cf0c333f3eb6bab1a0674f5052073
elcascavel <simao.ramaral@gmail.com>
2022-05-28 20:28:30 +0100

    Responsive navbar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,57 +1,120 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset(mix('css/app.css')) }}">
-        <link rel="icon" href="{{ url('css/cm-icon.svg') }}">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet">
 
-        <!-- Scripts -->
-        <script src="{{ asset(mix('js/app.js')) }}" defer></script>
-    </head>
-    <body class="antialiased bg-zinc-900 text-white">
-        <nav class="border-b border-zinc-700">
-            <div class="container mx-auto flex items-center justify-between px-4 py-6">
-                <ul class="flex items-center">
-                    <li>
-                        <a href="/laravel">
-                            <x-application-logo />
-                        </a>
-                    </li>
-                    <li class="ml-16">
-                        <a href="/laravel/equipa" class="font-semibold transition ease-in-out hover:text-red-300">Equipa</a>
-                    </li>
-                    <li class="ml-16">
-                        <a href="/laravel/merch" class="font-semibold transition ease-in-out hover:text-red-300">Merch</a>
-                    </li>
-                    <li class="ml-16">
-                        <a href="/laravel/links" class="font-semibold transition ease-in-out hover:text-red-300">Links</a>
-                    </li>
-                </ul>
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset(mix('css/app.css')) }}">
+    <link rel="icon" href="{{ url('css/cm-icon.svg') }}">
+
+    <!-- Scripts -->
+    <script src="{{ asset(mix('js/app.js')) }}" defer></script>
+</head>
+
+<body class="antialiased bg-zinc-900 text-white">
+    <nav x-data="{isOpen: false}" class="border-b border-zinc-700">
+        <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+            <div class="relative flex items-center justify-between">
                 <div class="flex items-center">
-                    <div class="ml-4">
-                    @if (Route::has('login'))
+                    <a href="/laravel" aria-label="Logo" title="Logo" class="mr-8">
+                    <p>logo</p>
+                    </a>
+                    <ul class="flex items-center space-x-8 lg:flex">
+                        <li><a href="equipa" aria-label="A nossa equipa" title="A nossa equipa"
+                                class="font-semibold transition ease-in-out hover:text-red-300">Equipa</a>
+                        </li>
+                        <li><a href="merch" aria-label="Merch" title="Merch"
+                                class="font-semibold transition ease-in-out hover:text-red-300">Merch</a>
+                        </li>
+                        <li><a href="links" aria-label="Links" title="Links"
+                                class="font-semibold transition ease-in-out hover:text-red-300">Links</a>
+                        </li>
+                    </ul>
+                </div>
+                <ul class="flex items-center space-x-8 lg:flex">
+                @if (Route::has('login'))
                     @auth
-                        <a href="{{ url('/dashboard') }}" class="text-gray-200 hover:text-red-300">{{ Auth::user()->name }}</a>
+                        <a href="{{ url('/dashboard') }}" class="tracking-wide text-gray-200 hover:text-red-300">{{ Auth::user()->name }}</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-500 hover:text-red-300">Log in</a>
+                        <a href="{{ route('login') }}" class="tracking-wide text-sm text-gray-500 hover:text-red-300">Log in</a>
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-500 hover:text-red-300">Register</a>
                         @endif
                     @endauth
             @endif
-                    </div>
+                </ul>
+                <!-- Mobile menu -->
+                <div class="lg:hidden">
+                    <button type="submit" @click="isOpen = !isOpen" aria-label="Open Menu" title="Open Menu"
+                        class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline">
+                        <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                            <path fill="currentColor"
+                                d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"></path>
+                            <path fill="currentColor"
+                                d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"></path>
+                            <path fill="currentColor"
+                                d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"></path>
+                        </svg>
+                    </button>
+        <div class="absolute top-0 left-0 w-full" x-show="isOpen" @click.away=" isOpen = false">
+          <div class="p-5 bg-zinc-900 border rounded shadow-sm">
+            <div class="flex items-center justify-between mb-4">
+              <div>
+                <a href="/" aria-label="Company" title="Company" class="inline-flex items-center">
+                  <svg class="w-8 text-deep-purple-accent-400" viewBox="0 0 24 24" stroke-linejoin="round" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" stroke="currentColor" fill="none">
+                    <rect x="3" y="1" width="7" height="12"></rect>
+                    <rect x="3" y="17" width="7" height="6"></rect>
+                    <rect x="14" y="1" width="7" height="6"></rect>
+                    <rect x="14" y="11" width="7" height="12"></rect>
+                  </svg>
+                  <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">Company</span>
+                </a>
+              </div>
+              <div>
+                <button aria-label="Close Menu" title="Close Menu" class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
+                  <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                    <path
+                      fill="currentColor"
+                      d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
+                    ></path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <nav>
+              <ul class="space-y-4">
+                <li><a href="/" aria-label="Our product" title="Our product" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Product</a></li>
+                <li><a href="/" aria-label="Our product" title="Our product" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Features</a></li>
+                <li><a href="/" aria-label="Product pricing" title="Product pricing" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Pricing</a></li>
+                <li><a href="/" aria-label="About us" title="About us" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">About us</a></li>
+                <li><a href="/" aria-label="Sign in" title="Sign in" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Sign in</a></li>
+                <li>
+                  <a
+                    href="/"
+                    class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                    aria-label="Sign up"
+                    title="Sign up"
+                  >
+                    Sign up
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
                 </div>
             </div>
-        </nav>
-        @yield('content')
-    </body>
-    </html>
+        </div>
+</nav>
+    @yield('content')
+</body>
+
+</html>
