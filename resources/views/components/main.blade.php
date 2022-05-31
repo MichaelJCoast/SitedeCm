9932fdbc5a5103404ca19e6ee7beedc1f1734524
elcascavel <simao.ramaral@gmail.com>
2022-05-31 20:22:53 +0100

    Mobile menu revamp

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -27,13 +27,13 @@
                     </a>
                     <ul class="hidden items-center space-x-8 lg:flex">
                         <li><a href="equipa" aria-label="A nossa equipa" title="A nossa equipa"
-                                class="font-semibold transition ease-in-out hover:text-red-300">Equipa</a>
+                                class="font-semibold transition ease-in-out">Equipa</a>
                         </li>
                         <li><a href="merch" aria-label="Merch" title="Merch"
-                                class="font-semibold transition ease-in-out hover:text-red-300">Merch</a>
+                                class="font-semibold transition ease-in-out">Merch</a>
                         </li>
                         <li><a href="links" aria-label="Links" title="Links"
-                                class="font-semibold transition ease-in-out hover:text-red-300">Links</a>
+                                class="font-semibold transition ease-in-out">Links</a>
                         </li>
                     </ul>
                 </div>
@@ -41,22 +41,22 @@
                 @if (Route::has('login'))
                     @auth
                     <li>
-                        <a href="{{ url('/dashboard') }}" class="tracking-wide text-gray-200 hover:text-red-300">{{ Auth::user()->name }}</a>
+                        <a href="{{ url('/dashboard') }}" class="tracking-wide text-gray-200">{{ Auth::user()->name }}</a>
                     </li>
                     @else
                         <li>
-                            <a href="{{ route('login') }}" class="tracking-wide text-sm text-gray-500 hover:text-red-300">Log in</a>
+                            <a href="{{ route('login') }}" class="tracking-wide text-sm text-gray-500">Log in</a>
                         </li>
                         @if (Route::has('register'))
                         <li>
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-500 hover:text-red-300">Register</a>
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-500">Register</a>
                         </li>
                         @endif
                     @endauth
             @endif
                 </ul>
                 <!-- Mobile menu -->
-                <div class="lg:hidden">
+                <div class="lg:hidden z-10">
                     <button type="submit" @click="isOpen = !isOpen" aria-label="Open Menu" title="Open Menu"
                         class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline">
                         <svg class="w-6 text-white" viewBox="0 0 24 24">
@@ -68,44 +68,42 @@
                                 d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"></path>
                         </svg>
                     </button>
-        <div class="absolute top-0 left-0 w-full" x-show="isOpen" @click.away="isOpen = false">
-          <div class="p-5 bg-zinc-900 border rounded shadow-sm">
+        <div class="absolute left-0 right-0 top-0" x-show="isOpen" @click.away="isOpen = false">
+          <div class="p-5 bg-black/90 rounded shadow-sm">
             <div class="flex items-center justify-between mb-4">
-              <div>
-                <a href="/laravel" aria-label="NECM" title="NECM" class="inline-flex items-center">
-                  <span class="text-xl font-bold tracking-wide uppercase">NECM</span>
-                </a>
-              </div>
-              <div>
-                <button type="submit" @click="isOpen = !isOpen" aria-label="Close Menu" title="Close Menu" class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
-                  <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                <button type="submit" @click="isOpen = !isOpen" aria-label="Close Menu" title="Close Menu" class="p-2 -mt-2 transition duration-200 rounded focus:outline-none focus:shadow-outline">
+                  <svg class="w-5 text-white" viewBox="0 0 24 24">
                     <path
                       fill="currentColor"
                       d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
                     ></path>
                   </svg>
                 </button>
-              </div>
             </div>
             <nav>
-              <ul class="space-y-4">
-                <li><a href="equipa" class="font-medium tracking-wide transition-colors duration-200 hover:text-deep-purple-accent-400">Equipa</a></li>
-                <li><a href="merch" class="font-medium tracking-wide transition-colors duration-200 hover:text-deep-purple-accent-400">Merch</a></li>
-                <li><a href="links" class="font-medium tracking-wide transition-colors duration-200 hover:text-deep-purple-accent-400">Links</a></li>
+              <ul class="space-y-6">
+                <li><a href="equipa" class="font-bold text-xl tracking-wide">Equipa</a></li>
+                <li><a href="merch" class="font-bold text-xl tracking-wide">Merch</a></li>
+                <li><a href="links" class="font-bold text-xl tracking-wide">Links</a></li>
                 @if (Route::has('login'))
                     @auth
+                    <div class="flex items-center justify-center">
                     <li>
-                        <a href="{{ url('/dashboard') }}" class="tracking-wide text-gray-200 hover:text-red-300">{{ Auth::user()->name }}</a>
+                        <a href="{{ url('/dashboard') }}" class="tracking-wide text-white">Bem-vindo, <span class="font-semibold">{{ Auth::user()->name }}</span></a>
                     </li>
+                    </div>
                     @else
+                        <div class="flex items-center justify-center">
                         <li>
-                            <a href="{{ route('login') }}" class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-sm text-gray-500 hover:text-red-300">Log in</a>
+                            <a href="{{ route('login') }}" class="px-6 font-medium tracking-wide text-sm">Log in</a>
                         </li>
                         @if (Route::has('register'))
                         <li>
-                            <a href="{{ route('register') }}" class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-sm text-gray-500 hover:text-red-300">Register</a>
+                            <a href="{{ route('register') }}" class="px-6 font-medium tracking-wide text-sm">Register</a>
                         </li>
                         @endif
+                        </div>
+                        
                     @endauth
             @endif
               </ul>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
     <x-carousel />
     <div class="container mx-auto px-8 sm:px-4 mb-28">
         <div class="latest-news">
-            <h2 class="mt-48 sm:mt-0 text-xl sm:text-3xl font-semibold py-6">Últimas Notícias</h2>
+            <h2 class="sm:mt-0 text-xl sm:text-3xl font-semibold py-6">Últimas Notícias</h2>
             @if(!$posts->isEmpty())
                 <div class="grid grid-cols-1 sm:grid-cols-3 gap-5">
                     @foreach($posts as $post)
