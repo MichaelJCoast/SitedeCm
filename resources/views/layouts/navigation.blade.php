b0dbfe2eb4f559b058b90f589a04572c119c9985
MMoreira01 <marco.pinto.moreira@gmail.com>
2022-06-01 18:11:29 +0100

    User Dropdown menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,7 +5,7 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="/laravel">
                         <x-application-logo class="block h-10 w-auto fill-current" />
                     </a>
                 </div>
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Profile') }}
                     </x-nav-link>
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Pedidos') }}
--- resources/views/requests.blade.php
+++ resources/views/requests.blade.php
@@ -1,10 +1,7 @@
-
-
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-white leading-tight">
-            {{ __('Profile') }}
-            
+            {{ __('Pedidos') }}
         </h2>
     </x-slot>
 
