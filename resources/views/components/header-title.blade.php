38f33b4ca57bcb4e2e53826ce12d8f4c9ef0068d
elcascavel <simao.ramaral@gmail.com>
2022-06-02 16:00:14 +0100

    front-end fixes

--- resources/views/components/header-title.blade.php
+++ resources/views/components/header-title.blade.php
@@ -1,3 +1,3 @@
-<a {{ $attributes->merge(['class' => 'sm:mt-0 text-2xl 2xl:text-4xl font-semibold py-6']) }}>
+<a {{ $attributes->merge(['class' => 'sm:mt-0 text-2xl 2xl:text-3xl font-semibold py-6']) }}>
     {{ $slot }}
 </a>
