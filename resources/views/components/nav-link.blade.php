af7e8be7640cc0d94da12b11ef5d2979afcef91c
elcascavel <simao.ramaral@gmail.com>
2022-06-03 20:15:22 +0100

    Added navbar border to selected items

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,7 +2,7 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'inline-flex cursor-pointer items-center px-1 pt-1 font-semibold leading-5 text-white hover:text-red-600 focus:outline-none transition duration-150 ease-in-out'
+            ? 'inline-flex cursor-pointer items-center px-1 pt-1 border-b-2 border-red-600 font-semibold leading-5 text-white hover:text-red-600 focus:outline-none transition duration-150 ease-in-out'
             : 'inline-flex cursor-pointer items-center px-1 pt-1 font-semibold leading-5 text-white hover:text-red-600 focus:outline-none transition duration-150 ease-in-out';
 @endphp
 
