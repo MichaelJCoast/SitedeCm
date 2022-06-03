b6d8e98c759fbcc58f07442d3e63dcd412b25959
elcascavel <simao.ramaral@gmail.com>
2022-06-03 14:33:04 +0100

    Fixed line height blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -3,7 +3,7 @@
             <img class="lg:w-[95%] 2xl:w-[65%] lg:mx-auto" src="../../laravel/{{$post->image}}" alt="post image">
             <div class="px-14 pb-14">
                 <p class="mt-6 text-sm sm:text-base font-semibold text-gray-300">{{$post->category}}</p>
-                <h2 class="mt-6 text-4xl sm:text-5xl text-white font-bold sm:first-line:leading-loose">
+                <h2 class="mt-6 text-4xl sm:text-5xl text-white font-bold sm:leading-normal">
                     {{$post->title}}
                 </h2>
                 <p class="mt-6 mb-6 text-xs sm:text-sm font-normal sm:font-semibold">
