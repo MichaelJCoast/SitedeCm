5ac24face2e4227601be2b2953b3b98132f45199
elcascavel <simao.ramaral@gmail.com>
2022-06-04 02:05:19 +0100

    Small fixes

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -5,6 +5,6 @@
     <div class="text-white p-6 space-y-4 text-base sm:text-lg text-justify">
         <h2 class="font-semibold text-3xl">Sobre o NECM</h2>
         <p>O <span class="font-semibold hover:text-red-600 transition duration-150 ease-in-out">Núcleo de Estudantes de Comunicação e Multimédia</span> tem como principal objetivo cooperar e dar voz a todos os alunos deste curso. A equipa que nos acompanha pretende dar sempre o seu melhor e, como tal, desenvolver um trabalho sério, organizado e responsável, para que toda a família de Comunicação e Multimédia se sinta acolhida e integrada.</p>
-        <p>Por forma a garantir uma excelente comunicação e entreajuda com todos, teremos sempre ao dispor o nosso e-mail e, também, todas as redes sociais, das quais dispomos informação.</p>
+        <p>De forma a garantir uma excelente comunicação e entreajuda com todos, teremos sempre ao dispor o nosso e-mail e, também, todas as redes sociais, das quais dispomos informação.</p>
     </div>
 </div>
