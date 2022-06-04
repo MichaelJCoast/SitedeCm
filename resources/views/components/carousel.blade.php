2799b42778992f1f2237b7b7eb3a0f9fa33809b1
elcascavel <simao.ramaral@gmail.com>
2022-06-04 01:10:46 +0100

    Carousel fix

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -2,7 +2,7 @@
     <div class="mx-auto my-auto">
         <img src="https://necm.utad.pt/wp-content/uploads/2021/12/WhatsApp-Image-2021-12-28-at-20.08.07-e1640816963642.jpeg" alt="">
     </div>
-    <div class="text-white p-6 space-y-4 text-base sm:text-lg text-justify">
+    <div class="text-white p-6 space-y-4 text-base sm:text-lg text-justify 2xl:w-2/4">
         <h2 class="font-semibold text-3xl">Sobre o NECM</h2>
         <p>O <span class="font-semibold hover:text-red-600 transition duration-150 ease-in-out">Núcleo de Estudantes de Comunicação e Multimédia</span> tem como principal objetivo cooperar e dar voz a todos os alunos deste curso. A equipa que nos acompanha pretende dar sempre o seu melhor e, como tal, desenvolver um trabalho sério, organizado e responsável, para que toda a família de Comunicação e Multimédia se sinta acolhida e integrada.</p>
         <p>Por forma a garantir uma excelente comunicação e entreajuda com todos, teremos sempre ao dispor o nosso e-mail e, também, todas as redes sociais, das quais dispomos informação.</p>
