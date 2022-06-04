74943d703e8a40dfce664763be993b77d188b0bb
elcascavel <simao.ramaral@gmail.com>
2022-06-04 01:04:32 +0100

    Main page carousel skeleton

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
 <div class="container mx-auto text-white">
             <img class="lg:w-[95%] xl:w-[75%] 2xl:w-[65%] mx-auto" src="../../laravel/{{$post->image}}" alt="post image">
-            <div class="px-14 pb-14 lg:px-52 xl:px-80">
+            <div class="px-8 pb-14 lg:px-52 xl:px-80">
                 <p class="mt-6 text-sm sm:text-base font-semibold text-gray-300">{{$post->category}}</p>
                 <h2 class="mt-6 text-4xl sm:text-3xl md:text-5xl lg:text-4xl text-white font-bold sm:leading-normal">
                     {{$post->title}}
