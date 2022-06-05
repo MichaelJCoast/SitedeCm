c3ac48e68df0f58c60418c7a9eddb697434f4bae
elcascavel <simao.ramaral@gmail.com>
2022-06-05 20:34:03 +0100

    Carousel text color fix

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -2,9 +2,9 @@
     <div class="">
         <img src="https://necm.utad.pt/wp-content/uploads/2021/12/WhatsApp-Image-2021-12-28-at-20.08.07-e1640816963642.jpeg" alt="">
     </div>
-    <div class="text-white p-6 space-y-4 text-base sm:text-lg text-justify">
-        <h2 class="font-semibold text-3xl">Sobre o NECM</h2>
-        <p class="text-gray-200">O <span class="font-semibold hover:text-red-600 transition duration-150 ease-in-out">Núcleo de Estudantes de Comunicação e Multimédia</span> tem como principal objetivo cooperar e dar voz a todos os alunos deste curso. A equipa que nos acompanha pretende dar sempre o seu melhor e, como tal, desenvolver um trabalho sério, organizado e responsável, para que toda a família de Comunicação e Multimédia se sinta acolhida e integrada.</p>
+    <div class="text-gray-200 p-6 space-y-4 text-base sm:text-lg text-justify">
+        <h2 class="font-semibold text-3xl text-white">Sobre o NECM</h2>
+        <p>O <span class="font-semibold hover:text-red-600 transition duration-150 ease-in-out">Núcleo de Estudantes de Comunicação e Multimédia</span> tem como principal objetivo cooperar e dar voz a todos os alunos deste curso. A equipa que nos acompanha pretende dar sempre o seu melhor e, como tal, desenvolver um trabalho sério, organizado e responsável, para que toda a família de Comunicação e Multimédia se sinta acolhida e integrada.</p>
         <p>De forma a garantir uma excelente comunicação e entreajuda com todos, teremos sempre ao dispor o nosso e-mail e, também, todas as redes sociais, das quais dispomos informação.</p>
     </div>
 </div>
