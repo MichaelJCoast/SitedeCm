d8b5f2c2797a6498722336925f0441e7a4e8f993
elcascavel <simao.ramaral@gmail.com>
2022-06-10 12:45:49 +0100

    Localization

---
+++ resources/lang/pt/profile.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Strings para o Perfil
+    |--------------------------------------------------------------------------
+    */
+
+    'update' => 'Atualizar',
+    'firstname' => 'Nome próprio',
+    'lastname' => 'Apelido',
+    'newPassword' => 'Nova password',
+    'confirmPassword' => 'Confirmar password',
+
+];
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
     <div class="container mx-auto px-8 md:px-14 mb-10 text-white space-y-6">
         @if(!$posts->isEmpty())
         <div class="mt-6">
-            <x-header-title :href="route('blog')">Últimas Notícias</x-header-title>
+            <x-header-title :href="route('blog')">{{__("index.newsTitle")}}</x-header-title>
         </div>  
                 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-6">
                     @foreach($posts as $post)
@@ -11,11 +11,11 @@
                     @endforeach
                 </div>
             <div class="text-center">
-                <a href="blog" class="font-semibold transition ease-in-out rounded-lg text-lg bg-transparent hover:bg-red-700 px-4 py-2">Ver mais notícias</a>
+                <a href="blog" class="font-semibold transition ease-in-out rounded-lg text-lg bg-transparent hover:bg-red-700 px-4 py-2">{{__("index.seeMore")}}</a>
             </div>
         @endif
         <div class="mt-6">
-            <x-header-title>Parcerias</x-header-title>
+            <x-header-title>{{__("index.partnersTitle")}}</x-header-title>
         </div>
     </div>
 </x-app-layout>
