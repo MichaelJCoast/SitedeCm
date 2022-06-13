7d43d42c5484bb7143ce3f6ba89e419b8ec3eb2e
elcascavel <simao.ramaral@gmail.com>
2022-06-13 13:11:39 +0100

    order view updates

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -1,45 +1,39 @@
 <x-app-layout>
-<head>
-<link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
-<link rel="stylesheet" href="https://unpkg.com/flowbite@1.4.7/dist/flowbite.min.css" />
-</head>
-<script src="https://unpkg.com/flowbite@1.4.7/dist/flowbite.js"></script>
-
-<div class="container mx-auto px-4 sm:px-8 max-w-3xl">
-    <div class="py-8">
-        <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
-            <div class="inline-block w-full shadow rounded-lg overflow-hidden">
-                <table class="w-full leading-normal">
+    <div class="container mx-auto max-w-3xl mt-6">
+                <table class="bg-white rounded-lg shadow-md">
                     <thead>
-                        <tr>
-                            <th scope="col" class="px-5 py-3  border-b border-gray-200 text-white  text-left text-sm uppercase font-normal">
+                        <tr class="bg-gray-100 border-b">
+                            <th scope="col" class="px-5 py-3 text-left text-sm font-semibold">
                                 Produto
                             </th>
-                            <th scope="col" class="px-5 py-3  border-b border-gray-200 text-white  text-left text-sm uppercase font-normal">
+                            <th scope="col" class="px-5 py-3 text-left text-sm font-semibold">
                                 Tamanho
                             </th>
-                            <th scope="col" class="px-5 py-3  border-b border-gray-200 text-white  text-left text-sm uppercase font-normal">
+                            <th scope="col" class="px-5 py-3 text-left text-sm font-semibold">
                                 Quantidade
                             </th>
-                            <th scope="col" class="px-5 py-3  border-b border-gray-200 text-white  text-left text-sm uppercase font-normal">
+                            <th scope="col" class="px-5 py-3 text-sm font-semibold">
                                 Preço
                             </th>
-                            <th scope="col" class="px-5 py-3  border-b border-gray-200 text-white  text-left text-sm uppercase font-normal">
-                                status
+                            <th scope="col" class="px-5 py-3 text-left text-sm font-semibold">
+                                Estado
+                            </th>
+                            <th scope="col" class="px-5 py-3 text-left text-sm font-semibold">
+                                Remover
                             </th>
                         </tr>
                     </thead>
 
                             <?php $total=0; ?>
-                        @foreach($order as $dataorder )
-                            <?php   $idmerch=$dataorder->product;
+                        @foreach($order as $order_data )
+                            <?php   $idmerch=$order_data->product;
                                     $merch = DB::table('merch')
                                     ->whereIn('id', [$idmerch] )
                                     ->get(); ?>
                             @foreach($merch as $key => $datamerch )
                         <tbody>
                         <tr>
-                            <td class="px-5 py-5 border-b border-gray-200  text-sm">
+                            <td class="px-5 py-5 border-b border-gray-200 text-sm">
                                 <div class="flex items-center">
                                     <div class="flex-shrink-0">
                                         <a href="#" class="block relative">
@@ -47,41 +41,41 @@
                                         </a>
                                     </div>
                                     <div class="ml-3">
-                                        <p class="text-white whitespace-no-wrap">
+                                        <p class="whitespace-no-wrap">
                                             {{$datamerch->name}}
                                         </p>
                                     </div>
                                 </div>
                             </td>
                             <td class="px-5 py-5 border-b border-gray-200 text-sm">
-                                <p class="text-white whitespace-no-wrap">
-                                {{$dataorder->size}}
+                                <p class=" whitespace-no-wrap">
+                                {{$order_data->size}}
                                 </p>
                             </td>
                             <td class="px-5 py-5 border-b border-gray-200 text-sm">
-                                <p class="text-white whitespace-no-wrap">
-                                {{$dataorder->quantity}}
+                                <p class=" whitespace-no-wrap">
+                                {{$order_data->quantity}}
                                 </p>
                             </td>
                             <td class="px-5 py-5 border-b border-gray-200 text-sm">
-                                <p class="text-white whitespace-no-wrap">
+                                <p class=" whitespace-no-wrap">
                                 {{$datamerch->price}}€
                                 <?php $total=$total+$datamerch->price?>
                                 </p>
                             </td>
                             <td class="px-5 py-5 border-b border-gray-200 text-sm">
-                                <span class="relative inline-block px-3 py-1 font-semibold text-white leading-tight">
+                                <span class="relative inline-block px-3 py-1 font-semibold  leading-tight">
 
                                     <span class="relative">
                                         <?php 
-                                        if(($dataorder->status)==0){
+                                        if(($order_data->status)==0){
                                             
                                             echo "Reserva Pendente";
                                         }
-                                        elseif (($dataorder->status)==1){
+                                        elseif (($order_data->status)==1){
                                             echo "Pagamento Pendente";
                                         }
-                                        elseif (($dataorder->status)==2){
+                                        elseif (($order_data->status)==2){
                                             echo "Levantamento Pendente";
                                         }
                                         else{
@@ -93,10 +87,9 @@
                             </td>
                             <td class="border-b border-gray-200 text-sm">
                             <form action="order" method="get">
-                            <input type="hidden" id="iddelete" name="iddelete" value="{{$dataorder->id}}">
-                            
-                            <button class="block text-white  hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center" type="submit" data-modal-toggle="popup-modal">
-                                <i class="las la-2x la-trash-alt"></i>
+                            <input type="hidden" id="iddelete" name="iddelete" value="{{$order_data->id}}">
+                            <button class="hover:bg-red-800 hover:text-white focus:ring-4 focus:outline-none focus:ring-red-500 font-medium rounded-lg text-sm p-3 text-center" type="submit" data-modal-toggle="popup-modal">
+                                <x-lineawesome-trash-alt class="w-8 h-8"></x-lineawesome-trash-alt>
                             </button>
                             
                             </form>
@@ -108,21 +101,21 @@
                 @endforeach
 
                         <tr> 
-                            <td class="px-5 py-5 border-b border-gray-200  text-sm">
+                            <td class="px-5 py-5text-sm">
                             </td>
-                            <td class="px-5 py-5 border-b border-gray-200 text-sm">
+                            <td class="px-5 py-5 text-sm">
                             </td>
-                            <td class="px-5 py-5 border-b border-gray-200 text-sm">
-                                <p class="text-white whitespace-no-wrap">
+                            <td class="px-5 py-5 text-sm">
+                                <p class=" whitespace-no-wrap">
                                Total:
                                 </p>
                             </td>
-                            <td class="px-5 py-5 border-b border-gray-200 text-sm">
-                                <p class="text-white whitespace-no-wrap">
-                                <?php echo "$total €" ?>
+                            <td class="px-5 py-5 text-sm">
+                                <p class=" whitespace-no-wrap">
+                                <?php echo "$total" . "€" ?>
                                 </p>
                             </td>
-                            <td class="px-5 py-5 border-b border-gray-200 text-sm">
+                            <td class="px-5 py-5 text-sm">
 
                             <form action="fatura" method="get">
                                 <button type="submit" class="inline-block px-6 py-2 border-2 border-red-600 text-red-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">Reservar Já</button>
@@ -133,9 +126,4 @@
 
                 </table>
             </div>
-        </div>
-    </div>
-</div>
-
-
 </x-app-layout>
