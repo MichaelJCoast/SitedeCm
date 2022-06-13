14a859306f3b07b4cb750819df94c0db36bae115
elcascavel <simao.ramaral@gmail.com>
2022-06-13 18:25:02 +0100

    front-end link update

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -1,8 +1,8 @@
 @props(['link'])
 
-<div class="p-6 border-l-4 border-red-500 bg-black">
-    <div class="flex items-center justify-between cursor-pointer">
-        <a href="{{$link->link_to}}" class="text-lg text-center font-medium text-white">
+<div class="p-6 border-l-4 border-red-600 rounded-lg bg-gray-50">
+    <div class="flex items-center justify-center cursor-pointer">
+        <a href="{{$link->link_to}}" class="text-xl font-semibold text-center text-black">
             {{$link->name}}
         </a>
 </div>
--- resources/views/links.blade.php
+++ resources/views/links.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
-    <div class="container px-6 sm:px-4 mt-4 mx-auto text-white">
-        <h1 class="font-bold text-center text-5xl">Links</h1>
+    <div class="mx-auto max-w-sm sm:max-w-xl">
+        <h1 class="font-bold text-center text-5xl mt-6 text-white">Links</h1>
         <div class="mt-4 grid grid-cols-1 gap-6">
             @foreach($links as $link)
                 <x-link :link="$link"></x-link>
