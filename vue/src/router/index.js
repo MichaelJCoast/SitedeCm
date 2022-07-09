47e00cd5e9d7683eecc32d7cf28ab1d43cb4d399
elcascavel <simao.ramaral@gmail.com>
2022-07-09 22:29:16 +0100

    Initial Vue configuration

---
+++ vue/src/router/index.js
@@ -0,0 +1,57 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from '../views/Home.vue';
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+import DefaultLayout from "../components/DefaultLayout.vue";
+import AuthLayout from "../components/AuthLayout.vue";
+
+import store from "../store";
+
+const routes = [
+    {
+        path: '/',
+        redirect: '/home',
+        component: DefaultLayout,
+        meta: {requiresAuth: true},
+        children: [
+            { path: "/home", name: "Home", component: Home },
+          ],
+    },
+    {
+        path: '/auth',
+        redirect: '/login',
+        name: 'Auth',
+        component: AuthLayout,
+        meta: {isGuest: true},
+        children: [
+            {
+                path: '/login',
+                name: 'Login',
+                component: Login
+            },
+            {
+                path: '/register',
+                name: 'Register',
+                component: Register
+            },
+        ]
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth && !store.state.user.token) {
+      next({ name: "Login" });
+    } else if (store.state.user.token && (to.meta.isGuest === 'Login' || to.meta.isGuest == 'Register')) {
+      next({name: 'Home'});
+    }
+    else {
+        next();
+    }
+  });
+
+export default router;
