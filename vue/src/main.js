fa2351d18aec429f12c7099fea3ecea51a3b1151
elcascavel <simao.ramaral@gmail.com>
2022-07-09 19:43:01 +0100

    Vue 3 installation

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { createApp } from 'vue'
 import store from './store'
 import router from './router'
@@ -8,3 +9,11 @@ createApp(App)
     .use(store)
     .use(router)
     .mount('#app')
+=======
+import { createApp } from "vue";
+import store from "./store";
+import './index.css'
+import App from "./App.vue";
+
+createApp(App).use(store).mount("#app");
+>>>>>>> 1cb0577 (Vue 3 installation)
--- vue/vite.config.js
+++ vue/vite.config.js
@@ -3,8 +3,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+<<<<<<< HEAD
   plugins: [vue()],
   devServer: {
     proxy: 'http://localhost:3000/',
 }
+=======
+  plugins: [vue()]
+>>>>>>> 1cb0577 (Vue 3 installation)
 })
