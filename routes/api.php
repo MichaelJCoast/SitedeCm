217b23c9ec68aec4b2acfe03ab60b78aaf9403e0
Simão Amaral <simao.ramaral@gmail.com>
2022-07-10 00:31:47 +0100

    Login fully working with Vue

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/register', [RegisteredUserController::class, 'register']);
+Route::post('/login', [RegisteredUserController::class, 'login']);
