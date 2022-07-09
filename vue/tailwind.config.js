47ccaf05003e451e8fe9cceda581a3aba0e4c26a
elcascavel <simao.ramaral@gmail.com>
2022-07-09 19:43:01 +0100

    Vue 3 installation

--- vue/tailwind.config.js
+++ vue/tailwind.config.js
@@ -2,12 +2,20 @@
 module.exports = {
   content: [
     "./index.html",
+<<<<<<< HEAD
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+=======
+    ".src/**/*.{vue,js,ts,jsx,tsx}",
+>>>>>>> 1cb0577 (Vue 3 installation)
   ],
   theme: {
     extend: {},
   },
+<<<<<<< HEAD
   plugins: [
     require('@tailwindcss/forms')
   ],
+=======
+  plugins: [],
+>>>>>>> 1cb0577 (Vue 3 installation)
 }
