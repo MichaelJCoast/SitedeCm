710b7d183f863c478c1dfcad5ad2bacbd8b683b7
MMoreira01 <86256848+MMoreira01@users.noreply.github.com>
2022-07-11 15:09:44 +0100

    Added the File Manager Feature

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
     use CrudTrait;
-    use \Backpack\CRUD\app\Models\Traits\CrudTrait, \Venturecraft\Revisionable\RevisionableTrait;
+    use \Backpack\CRUD\app\Models\Traits\CrudTrait;
 
     /*
     |--------------------------------------------------------------------------
@@ -33,9 +34,41 @@ class Post extends Model
     {
         $attribute_name = "image";
         $disk = "public";
-        $destination_path ="uploads";
+        $destination_path ="uploads/posts";
 
         $this->uploadFileToDisk($value, $attribute_name, $disk, $destination_path);
+
+         // if the image was erased
+         if ($value == null) {
+            // delete the image from disk
+            if ($this->{$attribute_name}) {
+                \Storage::disk($disk)->delete($this->{$attribute_name});
+            }
+
+            // set null in the database column
+            $this->attributes[$attribute_name] = null;
+        }
+
+        // if a base64 was sent, store it in the db
+        if (Str::startsWith($value, 'data:image')) {
+            // 0. Make the image
+            $image = \Image::make($value)->encode('jpg', 90);
+
+            // 1. Generate a filename.
+            $filename = md5($value.time()).'.jpg';
+
+            // 2. Store the image on disk.
+            \Storage::disk($disk)->put($destination_path.'/'.$filename, $image->stream());
+
+            // 3. Delete the previous image, if there was one.
+            \Storage::disk($disk)->delete($this->{$attribute_name});
+
+            // 4. Save the public path to the database
+            // but first, remove "public/" from the path, since we're pointing to it from the root folder
+            // that way, what gets saved in the database is the user-accesible URL
+            $public_destination_path = Str::replaceFirst('public/', '', $destination_path);
+            $this->attributes[$attribute_name] = $public_destination_path.'/'.$filename;
+        }
 
     // return $this->attributes[{$attribute_name}]; // uncomment if this is a translatable field
     }
