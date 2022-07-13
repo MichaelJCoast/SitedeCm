a24c76727d9108548baf857b5ae9bc23a06722e0
Simão Amaral <simao.ramaral@gmail.com>
2022-07-13 17:19:38 +0100

    Removed any Breeze login / register

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,18 +13,7 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                @php
-                    $profileViews = Route::current()->getName() == 'dashboard' || Route::current()->getName() == 'orders'
-                @endphp
                 
-                @if($profileViews)
-                        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                            {{ __('Perfil') }}
-                        </x-nav-link>
-                        <x-nav-link :href="route('orders')" :active="request()->routeIs('orders')">
-                            {{ __('Pedidos') }}
-                        </x-nav-link>
-                    @else
                         <x-nav-link :href="route('equipa')" :active="request()->routeIs('equipa')">
                             {{ __('Equipa') }}
                         </x-nav-link>
@@ -34,64 +23,13 @@
                         <x-nav-link :href="route('links')" :active="request()->routeIs('links')">
                             {{ __('Links') }}
                         </x-nav-link>
-                        @endif
 
                 </div>
             </div>
            
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
-            @if (Route::has('login'))
-                    @auth
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <button
-                            class="flex items-center text-white text-lg font-semibold hover:text-red-600 hover:border-gray-300 focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
-                            <div>{{ Auth::user()->name }}</div>
-
-                            <div class="ml-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd"
-                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                        clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                        </button>
-                    </x-slot>
-
-                    <x-slot name="content">
-                        <form method="GET" action="{{ route('dashboard') }}">
-                            @csrf
-                            <x-dropdown-link :href="route('dashboard')" onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Perfil') }}
-                            </x-dropdown-link>
-                        </form>
-                        @if(Auth::user()->hasRole('admin') || Auth::user()->hasRole('mod') )
-                            <!-- Authentication -->
-                            <form method="GET" action="{{ backpack_url() }}">
-                                @csrf
-
-                                <x-dropdown-link :href="backpack_url()" onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                    {{ __('Backoffice') }}
-                                </x-dropdown-link>
-
-                            </form>
-                        @endif
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-
-                            <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </x-dropdown-link>
-
-                        </form>
-                    </x-slot>
-                </x-dropdown>
-            </div>
+            
         </div>
         <!-- Hamburger -->
             <div class="-mr-2 flex items-center sm:hidden text-white">
@@ -123,60 +61,6 @@
             <x-responsive-nav-link :href="route('links')" :active="request()->routeIs('links')">
                 {{ __('Links') }}
             </x-responsive-nav-link>
-        </div>
-
-
-        <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-zinc-700">
-            <div class="px-4">
-            <div class="font-medium text-base text-white">{{ Auth::user()->name }}</div>
-                <div class="flex flex-wrap space-x-1">
-                </div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-            </div>
-
-            <div class="space-y-1">
-                <!-- Authentication -->
-                @if(Auth::user()->hasRole('admin'))
-                            <!-- Authentication -->
-                            <form method="GET" action="{{ backpack_url() }}">
-                                @csrf
-                        <x-responsive-nav-link :href="backpack_url()" onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Backoffice') }}
-                    </x-responsive-nav-link>
-
-                            </form>
-                @endif
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form>
-                
-            </div>
-            @else
-            <form method="GET" action="{{ route('login') }}">
-                @csrf
-                <x-responsive-nav-link :href="route('login')" onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Login') }}
-                    </x-responsive-nav-link>
-                </form>
-                 @if (Route::has('register'))
-                 <form method="GET" action="{{ route('register') }}">
-                    @csrf
-                 <x-responsive-nav-link :href="route('register')" onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Register') }}
-                    </x-responsive-nav-link> 
-                </form>
-                 @endif
-             @endauth
-         @endif
         </div>
     </div>
 </nav>
