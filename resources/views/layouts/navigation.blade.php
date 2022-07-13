cb6773c50e6028d8d5382a72a7175955542fac51
Simão Amaral <simao.ramaral@gmail.com>
2022-07-13 17:58:02 +0100

    Removed additional leftovers

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -49,9 +49,6 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Perfil') }}
-            </x-responsive-nav-link>
             <x-responsive-nav-link :href="route('equipa')" :active="request()->routeIs('equipa')">
                 {{ __('Equipa') }}
             </x-responsive-nav-link>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\PostCrudController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,147 +12,101 @@ use App\Http\Controllers\Admin\PostCrudController;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 /* Main Page */
 Route::get('/', function () {
     $post = DB::table('posts')
-    ->orderBy('created_at', 'desc')
-    ->take(6)
-    ->get();
-    
+        ->orderBy('created_at', 'desc')
+        ->take(6)
+        ->get();
+
     return view('index', ['posts' => $post]);
 });
 
 /* Equipa */
 Route::get('/equipa', function () {
     $team = DB::table('equipa')
-    ->orderBy('dep_id', 'ASC')
-    ->get();
+        ->orderBy('dep_id', 'ASC')
+        ->get();
     $dep = DB::table('departments')
-    ->orderBy('id', 'ASC')
-    ->get();
+        ->orderBy('id', 'ASC')
+        ->get();
     $roles = DB::table('cargos')
-    ->orderBy('id', 'ASC')
-    ->get();
-    return view('team',compact(['team' ,'dep','roles']));
+        ->orderBy('id', 'ASC')
+        ->get();
+    return view('team', compact(['team', 'dep', 'roles']));
 })->name('equipa');
 
 /* Merch */
 Route::get('/merch', function () {
     $merch = DB::table('merch')
-    ->get();
+        ->get();
     return view('merch', ['merch' => $merch]);
-    })->name('merch');
-
+})->name('merch');
 
 /* Product */
 
 Route::get('/merch/product', function () {
-    if(!isset($_GET['id'])){ 
-    return redirect()->route('merch'); } else{ 
-    $merch = DB::table('merch')
-    ->whereIn('id', [$_GET['id']])
-    ->get();  };
+    if (!isset($_GET['id'])) {
+        return redirect()->route('merch');} else {
+        $merch = DB::table('merch')
+            ->whereIn('id', [$_GET['id']])
+            ->get();};
 
     return view('product', ['merch' => $merch]);
-    })->middleware(['auth', 'verified']);
-
-
+});
 
 /* Order */
 Route::get('/order', function () {
     /* se o tamanho for nulo o valor fica como 0 */
-    if(!isset($_GET['size'])){ $_GET['size']=0; } else { }
+    if (!isset($_GET['size'])) {$_GET['size'] = 0;} else {}
     /* se o valor de iddelete existir, faz delete*/
-    if(!isset($_GET['iddelete'])){ } else{
-    DB::table('order')
-        ->where('id', ($_GET['iddelete']))
-        ->delete(); 
+    if (!isset($_GET['iddelete'])) {} else {
+        DB::table('order')
+            ->where('id', ($_GET['iddelete']))
+            ->delete();
         return redirect('/order');}
     /* se o valor de iddelete existir, faz delete*/
-        if(!isset($_GET['id'])){}
-        else{
-    DB::table('order')
-        ->insert(array('user'=> Auth::id(),'product'=> $_GET['id'],'size'=> $_GET['size'],'quantity'=>'1','status'=>'0',));
-        return redirect('/order'); }
+    if (!isset($_GET['id'])) {} else {
+        DB::table('order')
+            ->insert(array('user' => 1, 'product' => $_GET['id'], 'size' => $_GET['size'], 'quantity' => '1', 'status' => '0'));
+        return redirect('/order');}
     $order = DB::table('order')
-        ->whereIn('user',  [Auth::id()] )
-        ->where('status','==', 0)
+        ->where('status', '==', 0)
         ->get();
-        return view('order',['order' => $order] );
-        })->middleware(['auth', 'verified']);
-
+    return view('order', ['order' => $order]);
+});
 
 /* FATURA */
-Route::get('/fatura', function () {
-    $order = DB::table('order')
-        ->whereIn('user', [Auth::id()] )
-        ->where('status','==', 0)
-        ->get();  
-    DB::table('order')
-        ->where('user',[Auth::id()])
-        ->where('status', '0')
-        ->update(['status'=>'1']);  
-    $layout=DB::table('mail')
-        ->where('name', 'fatura' )
-        ->get();
-        \Mail::to( auth()->user()->email )->send(new \App\Mail\fatura($order, $layout));
-        return redirect()->route('merch');
-    });
 
-
-
-/* MAILS */
-Route::get('/mail', function () {    
-    $count=0;
-    $mail=DB::table('mail')
-        ->get();
-
-    if(!isset($_GET['id'])){        
-    $main=DB::table('mail')
-        ->get(); } 
-    else{ 
-    $main=DB::table('mail')
-        ->where('id', $_GET['id'] )
-        ->get();
-    $user = DB::table('users')
-        ->get();
-    foreach($user as $user){
-    \Mail::to( $user->email )->send(new \App\Mail\main($main)); 
-    $count+=1; }
-    dd("Email Enviado para $count pessoas"); };
-    return view('mail', ['mail' => $mail]); })
-        ->middleware('auth');
-
-
+/* Route::get('/fatura', function () {
+$order = DB::table('order')
+->whereIn('user', [Auth::id()])
+->where('status', '==', 0)
+->get();
+DB::table('order')
+->where('user', [Auth::id()])
+->where('status', '0')
+->update(['status' => '1']);
+$layout = DB::table('mail')
+->where('name', 'fatura')
+->get();
+\Mail::to(auth()->user()->email)->send(new \App\Mail\fatura($order, $layout));
+return redirect()->route('merch');
+}); */
 
 /* Post */
 Route::get('blog/{post:slug}', [PostCrudController::class, 'showPosts']);
 
 Route::get('/blog', [PostCrudController::class, 'blogPostIndex'])->name('blog');
 
-
 /* Links */
 Route::get('/links', function () {
     $link = DB::table('links')
-    ->latest()
-    ->get();
-    return view('links', ['links' => $link]);
-    })->name('links');
-
-    
-Route::group(['middleware' => 'auth'], function() {
-    Route::get('/dashboard', function () {
-        $user = DB::table('users')
+        ->latest()
         ->get();
-        return view('dashboard',['user' => $user]);
-    })->name('dashboard');
+    return view('links', ['links' => $link]);
+})->name('links');
 
-    Route::put('dashboard', [\App\Http\Controllers\ProfileController::class, 'update'])
-    ->name('dashboard.update');
-
-    Route::view('pedidos', 'orders.index')->name('orders');
-});
-
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
