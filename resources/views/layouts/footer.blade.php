1f802a06d575f8cc7d5a4b60727931b54ea3c186
Simão Amaral <simao.ramaral@gmail.com>
2022-07-15 12:57:54 +0100

    Removed unused php views

--- resources/views/layouts/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<footer class="bg-black">
-        <div class="container px-8 md:px-14 mx-auto py-8">
-            <div class="flex justify-center space-x-6 mb-2">
-            </div>
-                <div class="grid grid-rows-1">
-                    <p class="mt-1 text-center text-xs text-white">Núcleo de Estudantes de Comunicação e Multimédia</p>
-                </div>
-            </div>
-    </footer>
