d14d3c0266028d30570c744dfae52e89f1d810f3
Simão Amaral <simao.ramaral@gmail.com>
2022-07-15 18:20:29 +0100

    Added controllers and API endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\AboutController;
 use App\Http\Controllers\DepartmentController;
 use App\Http\Controllers\LinksController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\StudentGroupRolesController;
 use App\Http\Controllers\TeamController;
 use Illuminate\Support\Facades\Route;
@@ -21,6 +23,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/team', [TeamController::class, 'index']);
 Route::get('/department', [DepartmentController::class, 'index']);
 Route::get('/studentgrouproles', [StudentGroupRolesController::class, 'index']);
+
+Route::get('/faq', [QuestionController::class, 'index']);
+Route::get('/about', [AboutController::class, 'index']);
 
 Route::get('/links', [LinksController::class, 'index']);
 
