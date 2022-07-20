ca3602f911fcdfa7cfd4f308aede3108a3ee3f7e
Simão Amaral <simao.ramaral@gmail.com>
2022-07-20 17:58:40 +0100

    Changed v-show to v-if

--- resources/js/views/Team.vue
+++ resources/js/views/Team.vue
@@ -22,8 +22,8 @@
                             <h2 v-if="getTeamMembersByDepartment(department, mandate).length" class="text-xl text-center font-semibold md:text-2xl mt-6">{{ department.name }}</h2>
                             <div class="grid gap-y-4 mt-8 grid-cols-1 sm:grid-cols-3 place-items-center">
                                 <div v-for="team_member in team_members" :key="team_member.id"
-                                    v-show="department.id === team_member.dep_id && mandate.id === team_member.mandato_id"
                                     class="w-full max-w-xs space-y-4 text-center">
+                                    <div v-if="department.id === team_member.dep_id && mandate.id === team_member.mandato_id">
                                     <img class="object-cover object-center w-[80%] h-auto mx-auto rounded-lg"
                                         :src="team_member.image" :alt="team_member.nome + ' photo'" />
                                     <h3 class="text-sm sm:text-lg font-semibold">{{ team_member.nome }}</h3>
@@ -32,6 +32,7 @@
                                             class="text-sm sm:text-lg text-red-500">
                                             {{ role.type }}
                                         </span>
+                                    </div>
                                     </div>
                                 </div>
                             </div>
