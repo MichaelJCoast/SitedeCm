382c0579d752ece55f7d69733f38deee8b3f1a9a
Simão Amaral <simao.ramaral@gmail.com>
2022-07-24 16:11:15 +0100

    Finalized cart view. Testing required

--- resources/js/views/Cart.vue
+++ resources/js/views/Cart.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="container min-h-screen mx-auto mb-6 lg:mt-6">
-    <div class="flex flex-row justify-between px-14">
-      <div class="flex flex-col justify-between w-full">
+  <div class="container min-h-screen mx-auto mb-6 mt-4 lg:mt-6">
+    <div class="flex flex-col lg:flex-row justify-between px-8 sm:px-14 space-y-4 sm:space-y-0">
+      <div class="flex flex-col justify-between w-full space-y-2">
         <div v-for="item in items" :key="item.id" :item="item" class="flex flex-row items-start text-white w-full">
-          <img class="w-40 h-auto" :src="item.photo" />
+          <img class="w-40 h-auto rounded-lg" :src="item.photo" />
           <div class="flex flex-col px-4 w-full text-neutral-200">
-            <div class="flex flex-row justify-between">
-              <h1 class="text-2xl font-bold">{{ item.name }}</h1>
-              <span class="text-2xl font-bold mr-6">{{ item_cost(item) }}€</span>
+            <div class="flex flex-col sm:flex-row sm:justify-between">
+              <h1 class="text-lg sm:text-2xl font-bold">{{ item.name }}</h1>
+              <span class="text-base sm:text-2xl font-bold mr-6">{{ item_cost(item) }}€</span>
             </div>
             <p class="text-sm">{{ product_total(item) + 'x' }}</p>
             <a @click="removeFromCart(item)" class="underline font-semibold text-neutral-200 cursor-pointer">Remover</a>
           </div>
         </div>
       </div>
-      <div class="text-neutral-200 flex flex-col bg-black px-8 py-6 space-y-4">
+      <div class="text-neutral-300 flex flex-col bg-black px-8 py-6 space-y-4">
         <h3 class="text-2xl font-semibold">Sumário</h3>
         <p class="text-neutral-100">Para efetuar a encomenda precisamos dos dados abaixo.</p>
         <input type="text" id="first_name" class="bg-gray-50" placeholder="Primeiro e Último Nome" />
@@ -23,7 +23,7 @@
           <p class="text-2xl">Total</p>
           <p class="text-2xl font-black">{{cart_total.toFixed(2) + '€'}}</p>
         </div>
-        <button class="bg-red-600 px-4 py-2">Efetuar Encomenda</button>
+        <button class="bg-red-600 px-4 py-2 uppercase font-semibold rounded-lg hover:bg-red-500 transition ease-in-out">Efetuar Encomenda</button>
       </div>
     </div>
   </div>
