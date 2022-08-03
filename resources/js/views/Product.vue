c17a1dc3f661fcb82da5d2893f9d6b0dadc6aac9
peixotomdb <peixotomdb@gmail.com>
2022-08-03 03:49:00 +0100

    product page clean code

--- resources/js/views/Product.vue
+++ resources/js/views/Product.vue
@@ -3,44 +3,28 @@
 <Loading v-if="loading"/>
     <div v-else class="grid grid-flow-row lg:grid-flow-col">
       <img class="h-96 mx-auto" :src="'../' + product.photo" :alt="product.name + ' image'">
-      <div class="text-white mt-6 px-8 md:px-14 space-y-4 md:w-3/4">
-        <h2 class="font-semibold text-xl">{{ product.name }}</h2>
-        <p v-html="product.description" class="text-gray-400"></p>
+      <div class="text-white mt-6 space-y-4 margin-0 lg:text-left text-center">
+        <p class="font-semibold text-xl capitalize">{{ product.name }}</p>
+        <p v-html="product.description" class="text-gray-400 capitalize"></p>
         <p class="font-semibold text-lg">€{{ product.price }}</p>
-          <RadioGroup class="flex flex-row" v-model="size">
-        <RadioGroupLabel class="sr-only">Tamanhos</RadioGroupLabel>
+        <RadioGroup v-model="size">
           <RadioGroupOption
             as="button"
-            v-for="size in product.sizes"
-            :key="size.id"
+            v-for="(size, i) in product.sizes"
+            :key="i"
             :value="size.name"
-            v-slot="{ checked }"
-          >
-            <div
-              :class="[
-                checked ? 'border border-white bg-white' : 'border border-white',
-              ]"
-              class="relative flex cursor-pointer px-6 py-4 shadow-md focus:outline-none"
-            >
-              <div class="flex w-full items-center justify-between">
-                <div class="flex items-center">
-                  <div class="text-sm">
-                    <RadioGroupLabel
-                      as="p"
-                      :class="checked ? 'text-neutral-900' : 'text-white'"
-                      class="font-semibold text-lg"
-                    >
-                      {{ size.name }}
-                    </RadioGroupLabel>
-                  </div>
-                </div>
-              </div>
-            </div>
+            v-slot="{ checked }" >
+          <button
+              class="border px-6 py-4 w-full shadow-md font-semibold p.0"
+              :class="checked ? 'text-black bg-white' : 'text-white'">
+              {{ size.name }}
+          </button>
           </RadioGroupOption>
       </RadioGroup>
       <button @click="addToCart()" type="submit" class="p-4 rounded-md uppercase bg-red-700 hover:brightness-125">
         <p class="font-bold text-gray-100">Adicionar ao carrinho</p>
       </button>
+        <span v-if="clicked" class="text-green-400 text-5xl align-middle pl-5">&#10003; </span>
       </div>    
     </div>
   </div>
@@ -51,12 +35,8 @@ import { computed } from "vue";
 import { useStore } from "vuex";
 import { useRoute } from "vue-router";
 import Loading from "../components/Loading.vue";
-import { ref } from 'vue'
-import {
-  RadioGroup,
-  RadioGroupLabel,
-  RadioGroupOption,
-} from '@headlessui/vue';
+import { ref } from "vue";
+import { RadioGroup, RadioGroupLabel, RadioGroupOption } from "@headlessui/vue";
 
 export default {
   data() {
@@ -65,14 +45,22 @@ export default {
     store.dispatch("getProductById", route.params.id);
 
     return {
+      clicked:false,
       product: computed(() => store.state.currentProduct),
       loading: computed(() => store.state.merch.loading),
-      size: ref('size.id'),
+      size: ref("size.id"),
     };
   },
   methods: {
     addToCart() {
-      this.$store.commit('addToCart', {product: this.product, selectedSize: this.size});
+      this.clicked=true,
+        setTimeout(() => {
+        this.clicked=false;
+        }, 800);
+      this.$store.commit("addToCart", {
+        product: this.product,
+        selectedSize: this.size,
+      });
     },
   },
   components: {
