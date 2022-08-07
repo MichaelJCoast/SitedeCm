6a59f31b48dce8d14b9680b6c7ffe53435273fb7
Simão Amaral <simao.ramaral@gmail.com>
2022-08-07 19:20:41 +0100

    Added Font Awesome to Link.vue

--- resources/js/components/LinkCard.vue
+++ resources/js/components/LinkCard.vue
@@ -1,22 +1,26 @@
 <template>
-      <a
-        :href="link.link_to"
-        class="text-xl font-semibold text-center text-gray-100 dark:text-gray-900 truncate capitalize"
-        target="_blank"
-      >
-  <div class="p-6 border border-neutral-700 dark:border-neutral-400 rounded-full bg-black dark:bg-neutral-200 hover:bg-opacity-40">
-    <div class="w-16 h-16 mt-[-18px] ml-[-16px] absolute rounded-full border border-neutral-700 dark:border-neutral-400">
-      <img src="https://img.icons8.com/plasticine/344/twitter.png">
+  <a :href="link.link_to" class="text-xl font-semibold text-center text-gray-100 dark:text-gray-900 truncate capitalize"
+    target="_blank">
+    <div
+      class="p-6 border border-neutral-700 dark:border-neutral-400 rounded-full bg-black dark:bg-neutral-200 hover:bg-opacity-40">
+      <div class="flex items-center cursor-pointer">
+          <font-awesome-icon class="h-10 w-10 mr-auto" :icon="link.link_icon" />
+        <p class="flex-1">{{ link.name }}</p>
+      </div>
     </div>
-    <div class="flex items-center justify-center cursor-pointer">
-        {{ link.name }}
-    </div>
-  </div>
   </a>
 
 </template>
 
 <script setup>
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+
+library.add(fab, fas, far)
+
 const { link } = defineProps({
   link: Object,
 });
