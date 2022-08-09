2b2a990aa21138a6e18ce460810acb1256634f4c
Simão Amaral <simao.ramaral@gmail.com>
2022-08-09 13:05:03 +0100

    Added meta tags, increasing our SEO to 100!

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="author" content="Núcleo de Estudantes de Comunicação e Multimédia">
+    <meta name="title" content="Núcleo de Estudantes de Comunicação e Multimédia">
+    <meta name="description" content="Sítio web oficial do Núcleo de Estudantes de Comunicação e Multimédia da UTAD">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
