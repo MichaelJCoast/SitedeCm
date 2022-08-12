e96f37bc934e09ad152bac368db3c97748eef6a8
Simão Amaral <simao.ramaral@gmail.com>
2022-08-12 14:42:18 +0100

    Add a margin to footer

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="bg-white dark:bg-black border-t border-neutral-400 dark:border-neutral-700">
+    <footer class="bg-white mt-4 dark:bg-black border-t border-neutral-400 dark:border-neutral-700">
         <div class="container px-8 md:px-14 mx-auto py-8">
             <div class="flex justify-center items-center space-x-1">
                 <p class="text-center text-xs text-neutral-900 dark:text-white">Criado com </p>
